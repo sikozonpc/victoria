a2c84efe4e919667e6ab966da0a36c970278c188
Tiago Ferreira <sikozonbatata@gmail.com>
2020-01-11 13:23:17 +0000

    chore(exports): default exports

---
+++ src/index.js
@@ -0,0 +1,3 @@
+export { default as Button } from './components/Button'
+export { ButtonBase } from './components/ButtonBase'
+export { ThemeConsumer, ThemeContext, ThemeProvider, createTheme, defaultTheme, useTheme, withTheme } from './components/theme/index'
